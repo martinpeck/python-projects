94db664adf2ed1332dbaeced8ac6f1c3a7148d94
Martin Peck <mpeck@hotmail.co.uk>
2015-05-23 16:31:16 +0100

    fixed minor typo

--- whos-in-space.py
+++ whos-in-space.py
@@ -8,7 +8,7 @@ json_data = json.loads(data)
 number_of_people = json_data['number']
 people_in_space = json_data['people']
 
-print("There are", number_of_people, "in space right now...")
+print("There are", number_of_people, "people in space right now...")
 
 for astronaut in people_in_space:
   print(astronaut['name'], "is on the following space craft:", astronaut['craft'])
