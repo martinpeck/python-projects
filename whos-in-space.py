36f73ad25699d1afbd188bd6684a4ba0890ad05e
Martin Peck <mpeck@hotmail.co.uk>
2015-05-23 16:28:02 +0100

    Added script to show who is in space right now

---
+++ whos-in-space.py
@@ -0,0 +1,14 @@
+import urllib.request
+import json
+
+response = urllib.request.urlopen('http://api.open-notify.org/astros.json')
+data = response.readall().decode('utf-8')
+json_data = json.loads(data)
+
+number_of_people = json_data['number']
+people_in_space = json_data['people']
+
+print("There are", number_of_people, "in space right now...")
+
+for astronaut in people_in_space:
+  print(astronaut['name'], "is on the following space craft:", astronaut['craft'])
